e2f5080d0d097c6f0e4c61dfb1af53a65b147786
obb12 <omar.bb@hotmail.fi>
2014-11-24 11:27:30 +0200

    Foo

--- index.php
+++ index.php
@@ -1,55 +1,47 @@
-<style type="text/css">
-.div-table{
-  display:table;         
-  width:auto;         
-  background-color:#eee;         
-  border:1px solid  #666666;         
-  border-spacing:5px;/*cellspacing:poor IE support for  this*/
-}
-.div-table-row{
-  display:table-row;
-  width:auto;
-  clear:both;
-}
-.div-table-col{
-  float:left;/*fix for  buggy browsers*/
-  display:table-column;         
-  width:200px;         
-  background-color:#ccc;  
-}
-.blue
-{
-       
-  background:blue;  
-}
-button
-{
-       
-  border-radius: 10px; 
-}
-
+<style>
 .white
 {
-       
-  background:white;  
+background-color:white;
 }
-</style>
-<div class="divTable">
-             <div class="headRow">
-                <div class="divCell" style="position:absolute;
-                top:10px; left:450px;">Vibra
-                <br><button type="button" id="1" class="white">On</button><br><button type="button"  id="2" class="white">On</button><br><button type="button" id="3" class="white">On</button><br><button type="button" id="4" class="white">On</button></div>
-                <div  class="divCell">Username</div>
-                <div  class="divCell">Gear 1</div>
-             </div>
-            <div class="divRow">
-                  <div class="divCell">Gear 2</div>
-                <div class="divCell">Gear 3</div>
-                <div class="divCell">Gear 4</div>
-            </div>
-            <span></span>
 
-      </div>
+
+.blue{
+background-color:blue;
+}
+
+
+button {
+border-radius:10px;
+}
+
+</style>
+<body>
+
+<table style="width:100%">
+  <tr>
+    <td>Gear 1</td>	
+    <td><button type="button" id="1" class="white">On</button></td>
+  </tr>
+  <tr>
+    <td>Gear 2</td>
+    <td><button type="button"  id="2" class="white">On</button></td>
+  </tr>
+  <tr>
+    <td>Gear 3</td>
+    <td><button type="button"  id="3" class="white">On</button></td>
+  </tr>
+   <tr>
+    <td>Gear 4</td>
+    <td><button type="button"  id="4" class="white">On</button></td>
+  </tr>
+  <span></span>
+</table>
+
+
+
+
+
+             
       <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
       <script type="text/javascript">
 
@@ -59,12 +51,17 @@ button
       	$(this).removeClass('white');
       	$(this).addClass('blue');
         $url = "txt.php";
-        html = $(this).html();
-        id = $(this).attr('id');
-        txt = id + ' ' + html;
+		
+		txt = '';
+		$.each( $('button'), function(i, nappula) {
+			html = $(nappula).html();
+			id = $(nappula).attr('id');
+        txt += id + ' ' + html + '\r\n';
+});
+        
         console.log(txt);
         $.post("txt.php",{suggest:txt},function(result){
-      $("span").html(result);
+     // $("span").html(result);
 
     });
       	
@@ -74,12 +71,16 @@ button
       	$(this).removeClass('blue');
       	$(this).addClass('white');
         $url = "txt.php";
-        html = $(this).html();
-        txt = $(this).attr('id') + ' ' + html;
+        	txt = '';
+		$.each( $('button'), function(i, nappula) {
+			html = $(nappula).html();
+			id = $(nappula).attr('id');
+        txt += id + ' ' + html + '\r\n';
+		});
         console.log(txt);
         $.post("txt.php",{suggest:txt},function(result){
           console.log("got result = "+result);
-      $("span").html(result);
+     // $("span").html(result);
     });
         
       	 };
@@ -87,3 +88,4 @@ button
       })
 
       </script>
+</body>
