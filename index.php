65366bba5a05dca2480a3b33c75da6b1e6615f04
obb12 <omar.bb@hotmail.fi>
2014-11-24 19:40:50 +0200

    Revert "hello"
    This reverts commit 1ab3c9b490f909938cd4d6744cbb46456437e94b.

--- index.php
+++ index.php
@@ -1,89 +1,73 @@
-<!DOCTYPE html><html><head><style type="text/css">
-.div-table{
-  display:table;         
-  width:auto;         
-  background-color:#eee;         
-  border:1px solid  #666666;         
-  border-spacing:5px;/*cellspacing:poor IE support for  this*/
-}
-.div-table-row{
-  display:table-row;
-  width:auto;
-  clear:both;
-}
-.div-table-col{
-  float:left;/*fix for  buggy browsers*/
-  display:table-column;         
-  width:200px;         
-  background-color:#ccc;  
-}
-.blue
-{
-       
-  background:blue;  
-}
-button
-{
-       
-  border-radius: 10px; 
-}
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
 
-.white
-{
-       
-  background:white;  
-}
-</style>
-<meta charset="utf-8">
-<title>tyhjä</title>
-</head>
 <body>
-<div class="divTable">
-             <div class="headRow">
-                <div class="divCell" style="position:absolute;
-                top:10px; left:450px;">Vibra
-                <br><button type="button" id="1" class="white">On</button><br><button type="button"  id="2" class="white">On</button><br><button type="button" id="3" class="white">On</button><br><button type="button" id="4" class="white">On</button></div>
-                <div  class="divCell">Username</div>
-                <div  class="divCell">Gear 1</div>
-             </div>
-            <div class="divRow">
-                  <div class="divCell">Gear 2</div>
-                <div class="divCell">Gear 3</div>
-                <div class="divCell">Gear 4</div>
-            </div>
-            <span></span>
 
-      </div>
+<table style="width:100%">
+  <tr>
+    <td>Gear 1</td>	
+    <td><button type="button" id="1" class="btn btn-default">On</button></td>
+  </tr>
+  <tr>
+    <td>Gear 2</td>
+    <td><button type="button"  id="2" class="btn btn-default">On</button></td>
+  </tr>
+  <tr>
+    <td>Gear 3</td>
+    <td><button type="button"  id="3" class="btn btn-default">On</button></td>
+  </tr>
+   <tr>
+    <td>Gear 4</td>
+    <td><button type="button"  id="4" class="btn btn-default">On</button></td>
+  </tr>
+  <span></span>
+</table>
+
+
+
+
+
+             
       <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
       <script type="text/javascript">
 
       $('button').click(function  () {
       	if ($(this).html()=='On') {
       		$(this).html('Off');
-      	$(this).removeClass('white');
-      	$(this).addClass('blue');
+      	$(this).removeClass('btn-default');
+      	$(this).addClass('btn-primary');
         $url = "txt.php";
-        html = $(this).html();
-        id = $(this).attr('id');
-        txt = id + ' ' + html;
+		
+		txt = '';
+		$.each( $('button'), function(i, nappula) {
+			html = $(nappula).html();
+			id = $(nappula).attr('id');
+        txt += id + ': ' + html + '\r\n';
+
+});
+        
         console.log(txt);
         $.post("txt.php",{suggest:txt},function(result){
-      $("span").html(result);
+     // $("span").html(result);
 
     });
       	
       	}
       	 else{
       	 	$(this).html('On');
-      	$(this).removeClass('blue');
-      	$(this).addClass('white');
+      	$(this).removeClass('btn-primary');
+      	$(this).addClass('btn-default');
         $url = "txt.php";
-        html = $(this).html();
-        txt = $(this).attr('id') + ' ' + html;
+        	txt = '';
+		$.each( $('button'), function(i, nappula) {
+			html = $(nappula).html();
+			id = $(nappula).attr('id');
+        txt += id + ': ' + html + '\r\n';
+        
+		});
         console.log(txt);
         $.post("txt.php",{suggest:txt},function(result){
-          console.log("got result = "+result);
-      $("span").html(result);
+          
+     // $("span").html(result);
     });
         
       	 };
@@ -92,4 +76,3 @@ button
 
       </script>
 </body>
-</html>
